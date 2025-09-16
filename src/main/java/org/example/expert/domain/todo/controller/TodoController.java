6ff67bce3dceb14e30950bfce29103e2f31128f0
seyoung5744 <seyoung574458@gmail.com>
2025-09-16 12:53:04 +0900

    3. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -2,8 +2,10 @@ package org.example.expert.domain.todo.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.todo.dto.TodoSearchCond;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
@@ -12,9 +14,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
+
+    private static final long END_DATE_OFFSET_DAYS = 1L;
 
     private final TodoService todoService;
 
@@ -29,13 +37,25 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @ModelAttribute TodoSearchCond todoSearchCond
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        validateDateRange(todoSearchCond.startDate(), todoSearchCond.endDate());
+
+        LocalDateTime startDateTime = (todoSearchCond.startDate() != null) ? todoSearchCond.startDate().atStartOfDay() : null;
+        LocalDateTime endDateTime = (todoSearchCond.startDate() != null) ? todoSearchCond.endDate().atStartOfDay().plusDays(END_DATE_OFFSET_DAYS) : null;
+
+        return ResponseEntity.ok(todoService.getTodos(page, size, todoSearchCond.weather(), startDateTime, endDateTime));
     }
 
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
+    }
+
+    private void validateDateRange(LocalDate startDate, LocalDate endDate) {
+        if (startDate != null && endDate != null && endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("endDate must not be before startDate");
+        }
     }
 }
