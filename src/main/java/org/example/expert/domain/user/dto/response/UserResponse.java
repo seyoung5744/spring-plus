52343a68f1bba20d2dcaf7d7b9b049728a92c168
seyoung5744 <seyoung574458@gmail.com>
2025-09-22 15:55:45 +0900

    feat : 회원 정보 조회 시 프로필 정보 추가

--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -1,15 +1,24 @@
 package org.example.expert.domain.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class UserResponse {
-
     private final Long id;
     private final String email;
+    private final String nickname;
+    private final String profileUr;
 
-    public UserResponse(Long id, String email) {
+    public UserResponse(Long id, String email, String nickname, String profileUr) {
         this.id = id;
         this.email = email;
+        this.nickname = nickname;
+        this.profileUr = profileUr;
+    }
+
+    public UserResponse(Long id, String email) {
+        this(id, email, null, null);
     }
 }
